4c2018d5b0f5c5b27ea547ba8042b0d9a8e50f68
vovasq <vova.balsutev@mail.ru>
2019-04-15 12:08:53 +0300

    added server frontend view

--- lazyv-server/src/main/java/com/vovas/lazyv/server/ServerController.java
+++ lazyv-server/src/main/java/com/vovas/lazyv/server/ServerController.java
@@ -1,7 +1,5 @@
 package com.vovas.lazyv.server;
 
-
-import com.vovas.lazyv.server.NativeEasyKeySender;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
