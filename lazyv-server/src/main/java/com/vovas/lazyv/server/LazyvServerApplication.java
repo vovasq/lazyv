ee5c40d8fcc5ebe22d0108a17782baee83d1ebe3
vovasq <vova.balsutev@mail.ru>
2019-04-25 00:23:17 +0300

    static section for native calls uncommented

--- lazyv-server/src/main/java/com/vovas/lazyv/server/LazyvServerApplication.java
+++ lazyv-server/src/main/java/com/vovas/lazyv/server/LazyvServerApplication.java
@@ -9,24 +9,24 @@ import java.lang.reflect.Field;
 @SpringBootApplication
 public class LazyvServerApplication {
 
-//	private static final Logger LOG = Logger.getLogger(LazyvApp.class);
-//
-//	static {
-//		LOG.info("*************Starting LazyvApp************");
-//		LOG.info("Current java.library.path" + System.getProperty("java.library.path"));
-//		Field fieldSysPath = null;
-//		try {
-//			fieldSysPath = ClassLoader.class.getDeclaredField( "sys_paths" );
-//			fieldSysPath.setAccessible(true);
-//			fieldSysPath.set(null, null);
-//			System.setProperty( "java.library.path", "./");
-//			System.loadLibrary("native");
-//			LOG.info("After setting up java.library.path = " + System.getProperty("java.library.path"));
-//		} catch (Throwable e) {
-//			e.printStackTrace();
-//			LOG.error("Start was unsuccessful due to problems with native libs");
-//		}
-//	}
+	private static final Logger LOG = Logger.getLogger(LazyvApp.class);
+
+	static {
+		LOG.info("*************Starting LazyvApp************");
+		LOG.info("Current java.library.path" + System.getProperty("java.library.path"));
+		Field fieldSysPath = null;
+		try {
+			fieldSysPath = ClassLoader.class.getDeclaredField( "sys_paths" );
+			fieldSysPath.setAccessible(true);
+			fieldSysPath.set(null, null);
+			System.setProperty( "java.library.path", "./");
+			System.loadLibrary("native");
+			LOG.info("After setting up java.library.path = " + System.getProperty("java.library.path"));
+		} catch (Throwable e) {
+			e.printStackTrace();
+			LOG.error("Start was unsuccessful due to problems with native libs");
+		}
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LazyvServerApplication.class, args);
